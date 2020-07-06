6327f9f8389044e8fa03a1f58280c549d94741ef
Jim Freeman <freeman32@fastmail.com>
2020-07-06 15:47:18 -0400

    Add manifest generation action. Reorganize controls.

---
+++ metadata/generate-manifest.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+
+const categories = require('./categories.json');
+
+async function readDirectory(directory) {
+    try {
+        const listing = await promisify(fs.readdir)(directory);
+        
+        let manifest = [];
+
+        await Promise.all(listing.map(async itemName => {
+            const fullItemName = path.join(directory, itemName);
+            if (fs.lstatSync(fullItemName).isDirectory()) {
+                manifest = manifest.concat(await readDirectory(fullItemName));
+            } else if (/.json$/.test(fullItemName)) {
+                manifest.push(await readFile(directory, itemName));
+            }
+        }));
+
+        return manifest;
+    } catch(error) {
+        console.error(`Error reading directory ${directory}: ${error}`);
+    }
+}
+
+async function readFile(directory, filename) {
+    const fullFilename = path.join(directory, filename);
+    try {
+        const stringData = await promisify(fs.readFile)(fullFilename, { encoding: 'utf-8' });
+
+        const fileObj = JSON.parse(stringData);
+
+        return {
+            filename: fullFilename,
+            category: categories[directory] || 'Uncategorized',
+            name: fileObj.name,
+            instructions: fileObj.instructions
+        };
+    } catch(error) {
+        console.error(`Error reading file ${fullFilename}: ${error}`);
+    }
+}
+
+readDirectory('controls').then(manifest => fs.writeFileSync('./metadata/manifest.json', JSON.stringify(manifest)));
